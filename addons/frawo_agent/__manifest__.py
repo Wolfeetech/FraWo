3034efeeb44d4e6471b25b9fb578cb09575d8261
Wolf Studio <admin@hs27.internal>
2026-06-07 22:53:54 +0200

    feat(frawo_agent): installierbares Addon-Skeleton

---
+++ addons/frawo_agent/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "FraWo Agent",
+    "version": "17.0.1.0.0",
+    "summary": "Autonomer Task-Agent: formatiert neue Tasks nach CI via lokalem Ollama",
+    "author": "FraWo GbR",
+    "license": "LGPL-3",
+    "depends": ["project", "mail"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/config_params.xml",
+        "data/agent_queue_tag.xml",
+        "data/ir_cron.xml",
+        "views/agent_log_views.xml",
+    ],
+    "application": False,
+    "installable": True,
+}
