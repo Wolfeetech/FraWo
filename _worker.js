8e579f8db0c84d54b501275b87d4e9fcba232944
Wolf Studio <admin@hs27.internal>
2026-07-25 14:07:23 +0200

    fix(security): update CSP connect-src policy in _worker.js for AzuraCast stream & API endpoints

--- _worker.js
+++ _worker.js
@@ -14,7 +14,7 @@ async function handleRequest(request) {
 
   // Critical Security Headers
   newHeaders.set('X-Frame-Options', 'SAMEORIGIN')
-  newHeaders.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https: blob:; connect-src 'self' https://www.google-analytics.com; frame-ancestors 'self'; base-uri 'self'; form-action 'self'")
+  newHeaders.set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com data:; img-src 'self' data: https: blob:; connect-src 'self' https://funk.frawo-tech.de http://10.1.0.38:8000 http://10.1.0.38 https://www.google-analytics.com; frame-ancestors 'self'; base-uri 'self'; form-action 'self'")
   newHeaders.set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
   newHeaders.set('X-Content-Type-Options', 'nosniff')
   newHeaders.set('X-XSS-Protection', '1; mode=block')
